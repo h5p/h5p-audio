db78b8a80c7ca50be58d86c27392de61a3c1a90c
Frode Petterson <frode.petterson@amendor.com>
2013-04-30 09:58:15 +0200

    Changed audio size and improved visibility in CP. Fixed flash support for Audio. (Now works in FF!) Minor adjustment to flash support in Video.

--- scripts/audio.js
+++ scripts/audio.js
@@ -2,14 +2,14 @@ var H5P = H5P || {};
 
 /**
  * Constructor.
- * 
+ *
  * @param {object} params Options for this library.
  * @param {string} contentPath The path to our content folder.
  */
 H5P.Audio = function (params, contentPath) {
   this.params = params;
   this.contentPath = contentPath;
-  
+
   if (window['H5PEditor'] !== undefined) {
     // TODO: Clean up tmp stuff
     this.tmpPath = H5PEditor.filesPath + '/h5peditor/';
@@ -18,7 +18,7 @@ H5P.Audio = function (params, contentPath) {
 
 /**
  * Wipe out the content of the wrapper and put our HTML in it.
- * 
+ *
  * @param {jQuery} $wrapper Our poor container.
  */
 H5P.Audio.prototype.attach = function ($wrapper) {
@@ -29,12 +29,12 @@ H5P.Audio.prototype.attach = function ($wrapper) {
     this.attachFlash($wrapper);
     return;
   }
-  
+
   // Add supported source files.
   if (this.params.files !== undefined) {
     for (var i = 0; i < this.params.files.length; i++) {
       var file = this.params.files[i];
-    
+
       if (audio.canPlayType(file.mime)) {
         var source = document.createElement('source');
         source.src = (file.tmp !== undefined && file.tmp ? this.tmpPath : this.contentPath) + file.path;
@@ -43,27 +43,33 @@ H5P.Audio.prototype.attach = function ($wrapper) {
       }
     }
   }
-  
-  if (!audio.children) {
-    $wrapper.text('No supported audio files found.');
+
+  if (!audio.children.length) {
+    // Try flash
+    this.attachFlash($wrapper);
     return;
   }
-  
+
+  if (this.endedCallback !== undefined) {
+    audio.addEventListener('ended', this.endedCallback, false);
+  }
+
   audio.className = 'h5p-audio';
   audio.controls = this.params.controls === undefined ? true : this.params.controls;
   audio.autoplay = this.params.autoplay === undefined ? false : this.params.autoplay;
-  
+
   if (this.params.fitToWrapper === undefined || this.params.fitToWrapper) {
     audio.style.width = '100%';
     audio.style.height = '100%';
   }
-  
+
   $wrapper.html(audio);
+  this.audio = audio;
 };
 
 /**
  * Attaches a flash audio player to the wrapper.
- * 
+ *
  * @param {jQuery} $wrapper Our dear container.
  */
 H5P.Audio.prototype.attachFlash = function ($wrapper) {
@@ -76,31 +82,55 @@ H5P.Audio.prototype.attachFlash = function ($wrapper) {
       }
     }
   }
-  
+
   if (audioSource === undefined) {
     $wrapper.text('No supported audio files found.');
+    if (this.endedCallback !== undefined) {
+      this.endedCallback();
+    }
+    return;
   }
-  
+
   var options = {
     buffering: true,
     clip: {
       url: window.location.protocol + '//' + window.location.host + audioSource,
       autoPlay: this.params.autoplay === undefined ? false : this.params.autoplay,
-      autoBuffering: true,
       scaling: 'fit'
     },
-    plugins: {}
+    plugins: {
+      controls: null
+    }
   };
-  
+
   if (this.params.controls === undefined || this.params.controls) {
     options.plugins.controls = {
-      url: 'http://releases.flowplayer.org/swf/flowplayer.controls-tube-3.2.15.swf',
+      fullscreen: false,
       autoHide: false
     };
   }
-  
+
+  if (this.endedCallback !== undefined) {
+    options.clip.onFinish = this.endedCallback;
+    options.clip.onError = this.endedCallback;
+  }
+
   this.flowplayer = flowplayer($wrapper[0], {
     src: "http://releases.flowplayer.org/swf/flowplayer-3.2.16.swf",
     wmode: "opaque"
   }, options);
+};
+
+/**
+ * Stop the video. TODO: Rename to pause?
+ *
+ * @returns {undefined}
+ */
+H5P.Audio.prototype.stop = function () {
+  if (this.flowplayer !== undefined) {
+    this.flowplayer.stop().close().unload();
+  }
+  if (this.audio !== undefined) {
+    this.audio.pause();
+  }
 };
