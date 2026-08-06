1ca6abbf0a781b9bed96610db22415e5228a1e0a
MatsJensen <mats.jensen@h5p.group>
2026-08-06 15:09:06 +0200

    Revert "VA-1760 remove width from being set in js (#118)"
    This reverts commit 4aaeea7bfebbebf744969dc3eec8886a9e56670c.

--- scripts/audio.js
+++ scripts/audio.js
@@ -98,6 +98,7 @@ H5P.Audio = (function ($) {
     // Fit to wrapper
     if (this.params.fitToWrapper) {
       audioButton.css({
+        'width': '100%',
         'height': '100%'
       });
     }
